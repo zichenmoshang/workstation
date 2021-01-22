0177990de419e781833624d7d1fe395157a13110
wangye19920128@icloud.com <wangye19920128@icloud.com>
2021-01-22 11:14:03 +0800

    main

--- workstation/src/components/Layout/aside.vue
+++ workstation/src/components/Layout/aside.vue
@@ -1,9 +1,86 @@
 <template>
-  <div>aside</div>
+  <div>
+    <a-menu
+      v-model:openKeys="openKeys"
+      v-model:selectedKeys="selectedKeys"
+      mode="inline"
+      theme="dark"
+      :inline-collapsed="collapsed"
+    >
+      <a-menu-item key="0">
+        <PieChartOutlined />
+        <span>Option 0</span>
+      </a-menu-item>
+      <a-menu-item key="1">
+        <PieChartOutlined />
+        <span>Option 1</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <DesktopOutlined />
+        <span>Option 2</span>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <InboxOutlined />
+        <span>Option 3</span>
+      </a-menu-item>
+      <a-sub-menu key="sub1">
+        <template #title>
+          <span><MailOutlined /><span>Navigation One</span></span>
+        </template>
+        <a-menu-item key="5">Option 5</a-menu-item>
+        <a-menu-item key="6">Option 6</a-menu-item>
+        <a-menu-item key="7">Option 7</a-menu-item>
+        <a-menu-item key="8">Option 8</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <template #title>
+          <span><AppstoreOutlined /><span>Navigation Two</span></span>
+        </template>
+        <a-menu-item key="9">Option 9</a-menu-item>
+        <a-menu-item key="10">Option 10</a-menu-item>
+        <a-sub-menu key="sub3" title="Submenu">
+          <a-menu-item key="11">
+            Option 11
+          </a-menu-item>
+          <a-menu-item key="12">
+            Option 12
+          </a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+    </a-menu>
+  </div>
 </template>
 <script>
+import { reactive } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import {
+  PieChartOutlined,
+  MailOutlined,
+  DesktopOutlined,
+  InboxOutlined,
+  AppstoreOutlined
+} from "@ant-design/icons-vue";
 export default {
-  name: "Aside"
+  name: "Aside",
+  components: {
+    PieChartOutlined,
+    MailOutlined,
+    DesktopOutlined,
+    InboxOutlined,
+    AppstoreOutlined
+  },
+  setup() {
+    const { options } = useRouter()
+    console.log(options);
+    const data = reactive({
+      selectedKeys: ["1"],
+      openKeys: ["sub1"],
+      preOpenKeys: ["sub1"]
+    });
+    return {
+      data
+    };
+  }
 };
 </script>
 <style scoped></style>
